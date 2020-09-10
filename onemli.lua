b18ec11d062210dd57eb95c70533dcc9c74f79f5
AryenN <70106602+AryenNN@users.noreply.github.com>
2020-09-10 17:35:21 +0300

    Create onemli.lua

---
+++ onemli.lua
@@ -0,0 +1,2 @@
+Dosyanın içerisine 270 x 2070 çözünürlükte olan bir PNG dosyası yerleştirin ve ismini " ic " yapın . Çünkü Oyuncu Listesi fotoğrafı çok daha güzel görünecektir bu dosyayı içine atmanız yeterlidir .
+Tabi ki script çalıştırmak için de server.cfg dosyasının içerisine " start loadingscreen " yazmanız ve kaydetmeniz yeterlidir .
