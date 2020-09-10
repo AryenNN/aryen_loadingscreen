0e9e7ddc5a3613c8434a8378f06cf4df04fdde6a
AryenN <70106602+AryenNN@users.noreply.github.com>
2020-09-10 17:34:10 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+//-----------------AryenN-----------------
+var title = "Fivem-Turk.com";
+var link = "AryenN#0005";
+//---------------AryenN---------------
+
+
+document.getElementById('title').innerHTML = title;
+document.getElementById('link').innerHTML = link;
+ 
+
+console.log('Connected.')
